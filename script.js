2315c010f465c2de4412add2957d54e5e9d991c1
MichaelOppong <OppongM@gmail.com>
2024-05-10 17:15:57 +0000

    update instructions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 /* 
-1. Use the DOM to change the HTML of the header.
-2. Use the DOM to change the src of the image.
+1. Use JS to change the HTML of the header.
+2. Use JS to change the src of the image.
 */
