5d571aac27b204298a0d53449f7417e69b6cbfc8
anvzhang <165081556+anvzhang@users.noreply.github.com>
2024-05-13 19:24:05 +0000

    dibe

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 1. Use JS to change the HTML of the header.
 2. Use JS to change the src of the image.
 */
+
+document.querySelector("h1").innerHTML = "meow";
+
+document.querySelector("img").src = "https://media1.tenor.com/m/557GUrcXp14AAAAd/jinx-cat-javascript.gif";
